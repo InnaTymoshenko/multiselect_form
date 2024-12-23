e655d3eea80e64656536ab8bb8324335aa6aa528
InnaTymoshenko <innatymosh@gmail.com>
2024-12-23 13:47:21 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import CommonForm from './components/CommonForm'
+
+import './App.css'
+
+function App_New() {
+	return (
+		<div className="App_New">
+			<CommonForm />
+		</div>
+	)
+}
+
+export default App_New
