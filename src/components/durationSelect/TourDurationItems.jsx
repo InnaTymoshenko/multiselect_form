5b36dd6b5d0c2acc5183927c77de83a940c397a8
InnaTymoshenko <innatymosh@gmail.com>
2025-01-09 18:10:07 +0200

    created component TourDurationItems

---
+++ src/components/durationSelect/TourDurationItems.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useMouseEvents } from '../../method/useMouseEvents'
+import { useTourDurationStore } from '../../store/store'
+import { getDaysText, getNightsText } from '../../method/fn'
+
+import styles from './durationSelect.module.css'
+
+const TourDurationItems = ({ chooseDuration }) => {
+	const { tourDuration, selectDuration } = useTourDurationStore()
+	const { hoveredItem, handleMouseEnter, handleMouseLeave } = useMouseEvents()
+
+	return (
+		<div className={styles.listCountry}>
+			<ul style={{ listStyle: 'none', paddingLeft: '6px' }}>
+				{tourDuration.map((d, index) => {
+					const isDisabled =
+						!localStorage.getItem('selectedResort') || !JSON.parse(localStorage.getItem('selectedResort')).country
+
+					return (
+						<li
+							key={`nights-${d.nights}-days-${d.days}`}
+							style={{
+								backgroundColor:
+									selectDuration === index
+										? '#E2E8EF'
+										: hoveredItem === index
+										? '#F5F5F5'
+										: selectDuration === null && index === 5
+										? '#E2E8EF'
+										: '',
+								padding: '6px',
+								cursor: isDisabled ? 'none' : 'pointer'
+							}}
+							onClick={!isDisabled ? () => chooseDuration(index) : null}
+							onMouseEnter={() => handleMouseEnter(index)}
+							onMouseLeave={handleMouseLeave}
+						>
+							<span style={{ fontWeight: 'bolder' }}>{d.nights}</span>
+							{` ${getNightsText(d.nights)} / ${d.days} ${getDaysText(d.days)}`}
+						</li>
+					)
+				})}
+			</ul>
+		</div>
+	)
+}
+
+export default TourDurationItems
